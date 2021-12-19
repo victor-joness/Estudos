961968cbd90e6b5ef146e070bd8402d39ac26b07
Victor Mesquita <victorjonesmesquits@gmail.com>
2021-12-19 10:09:47 -0300

    Create function ArrayChallenge(arr).js

---
+++ ArrayChallenge - all LIS values/function ArrayChallenge(arr).js
@@ -0,0 +1,27 @@
+function ArrayChallenge(arr)
+{
+    let n = arr.length;
+    let lis = Array(n).fill(0);
+    let i, j, max = 0;
+ 
+    // Initialize LIS values for all indexes
+    for(i = 0; i < n; i++)
+        lis[i] = 1;
+ 
+    // Compute optimized LIS values in
+    // bottom up manner
+    for(i = 1; i < n; i++)
+        for(j = 0; j < i; j++)
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                lis[i] = lis[j] + 1;
+ 
+    // Pick maximum of all LIS values
+    for(i = 0; i < n; i++)
+        if (max < lis[i])
+            max = lis[i];
+ 
+    return max;
+}
+   
+// keep this function call here 
+console.log(ArrayChallenge([10,20,3,4,40]));
