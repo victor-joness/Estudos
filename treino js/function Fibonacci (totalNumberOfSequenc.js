c5289fb7dd48238d56dc333a4b7aa69291c81f0a
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-01-09 19:49:00 -0300

    Create function Fibonacci (totalNumberOfSequenc.js

---
+++ treino js/function Fibonacci (totalNumberOfSequenc.js
@@ -0,0 +1,14 @@
+function Fibonacci(totalNumberOfSequence) {
+    let fibArray = []
+
+    fibArray[0] = 0;
+    fibArray[1] = 1;
+
+    for (let i = 2; i <= totalNumberOfSequence; i++) {
+        fibArray[i] = fibArray[i - 2] + fibArray[i - 1]
+    }
+
+    return fibArray;
+}
+
+console.log(Fibonacci(5)) //just for safe test
