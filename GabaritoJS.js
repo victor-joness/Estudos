f3a20d232ded069de831aad4b5f17a1df6debb16
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-06 15:29:03 -0300

    Update GabaritoJS.js

--- GabaritoJS.js
+++ GabaritoJS.js
@@ -39,4 +39,34 @@ let points = [
 ]
 console.log(points[1]);
 
+let a = []; // Cria um array vazio
+a.push(1,2,3); // O método push() adiciona elementos em um array
+a.reverse(); // Outro método: inverte a ordem dos elementos
+console.log(a);
+
+//como declaramos points antes, temos que ele é o nosso objeto e dentro dele tem uma propiedade chamada dist que é uma função, que calcula distância entre nossos 2 pontos
+points.dist = function() { // Define um método para calcular a distância entre
+    // pontos
+    var p1 = this[0]; // Primeiro elemento do array que chamamos
+    var p2 = this[1]; // Segundo elemento do objeto "this"
+    var a = p2.x-p1.x; // Diferença em coordenadas X
+    var b = p2.y-p1.y; // Diferença em coordenadas Y
+    return Math.sqrt(a*a + // O teorema de Pitágoras
+    b*b); // Math.sqrt() calcula a raiz quadrada
+   };
+points.dist()
+console.log(points.dist())
+
+//um claro exemplo de como se fazer uma funcao em js, temos aqui uma funcao de fatorial
+function factorial(n) { // Uma função para calcular fatoriais
+    var product = 1; // Começa com o produto de 1
+    while(n > 1) { // Repete as instruções que estão em {}, enquanto a
+    product *= n; // Atalho para product = product * n;
+    n--; 
+    } 
+    return product; // Retorna o produto
+}
+console.log(factorial(4));
+
+
 
