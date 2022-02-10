3d05dfb94f5012438ddc65d5e763e87523e35d5d
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-10 08:49:27 -0300

    Update Basic Data Structures.js

--- JS do lado do cliente/Basic Data Structures.js
+++ JS do lado do cliente/Basic Data Structures.js
@@ -41,7 +41,49 @@ function popShift(arr) {
 }  
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
+//Remover itens usando splice(), splice()nos permite fazer exatamente isso: remover qualquer número de elementos consecutivos de qualquer lugar em um array.
+//splice()pode ter até 3 parâmetros, mais vamos usar 2, por enquanto , o indice do inicio e quantos depois do indice de inicio vamos retirar
+//assim como os outros, splice tambem retorna aquilo que removeu
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+let newArray = array.splice(3, 2);
+console.log(newArray);
+//aqui temos outro exemplo
+const arr = [2, 4, 5, 1, 7, 5, 2, 1]; //vamos remover da posição 1 e contar 4 elementos a partir dele e contando com ele.
+arr.splice(1,4)
+console.log(arr);
+
+//Adicionar itens usando splice(),Lembra no último desafio que mencionamos que splice()pode levar até três parâmetros? Bem, você pode usar o terceiro parâmetro, 
+//composto por um ou mais elementos, para adicionar ao array. 
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 1;
+numbers.splice(startIndex, amountToDelete, 13, 14);
+console.log(numbers); //removemos o 12 e adicionamos o 13 e 14
+
+//temos outro exemplo
+function htmlColorNames(arr) {
+    arr.splice(0,2,'DarkSalmon','BlanchedAlmond');
+    return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+//Copiar itens da matriz usando slice(),Em vez de modificar um array, slice()copia ou extrai um determinado número de elementos para um novo array, deixando o array chamado intocado.
+//slice()leva apenas 2 parâmetros — o primeiro é o índice no qual iniciar a extração e o segundo é o índice no qual interromper a extração 
+//(a extração ocorrerá até, mas não incluindo o elemento neste índice).
+//basicamento um indice de inicio e o de fim, contando apenas o de inicio
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+let todaysWeather = weatherConditions.slice(1, 3);
+console.log(weatherConditions);
+console.log(todaysWeather);
+//temos outro exemplo
+function forecast(arr) {
+    arr = arr.slice(2,4)
+    return arr;
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
 //
+
+
 
 
 
