950a4c90a28a78fa48bc95a70d83b8009db615d4
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-10 10:16:17 -0300

    Update Basic Data Structures.js

--- JS do lado do cliente/Basic Data Structures.js
+++ JS do lado do cliente/Basic Data Structures.js
@@ -128,7 +128,42 @@ function quickCheck(arr, elem) {
     }
 }
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+//Iterar através de todos os itens de uma matriz usando loops For
 
 
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i].indexOf(elem) == -1){
+          newArr.push(arr[i])
+      }
+    }
+    // Only change code above this line
+    return newArr;
+}
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([["amy", "beth", "sam"], ["dave", "sean", "peter"]], "peter"));
+//temos outro exemplo
+function greaterThanTen(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 10) {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+}
+//Crie arrays multidimensionais complexos
+let myNestedArray = [
+    // Only change code below this line
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    [['deep'],['concat', false, true, 'spread', 'array']],
+    [[['deeper'],['mutate', 1327.98, 'splice', 'slice', 'push']]],
+    [[[['deepest'],['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']]]]
+]
+//esse array tem 5 niveis de complexidade, poi são 5 array aninhados um dentro do outro;
 
+//Adicionar pares de valores-chave a objetos JavaScript
 
