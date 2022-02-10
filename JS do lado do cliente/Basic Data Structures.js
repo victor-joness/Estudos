a53243d5d9a26e13b16e2d1ca9fc00aa23953973
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-10 08:26:25 -0300

    Update Basic Data Structures.js

--- JS do lado do cliente/Basic Data Structures.js
+++ JS do lado do cliente/Basic Data Structures.js
@@ -1,1 +1,13 @@
-teste
+//basic data structures
+//Use uma matriz para armazenar uma coleção de dados, exemplo array unidimensional, que tem apenas um array, não temos nada aninhado
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
+//aceita, numbers, string, boleans, outros arrays, objetos
+let yourArray = [true, 10, "victor", 5, null];
+//Acesse o conteúdo de um array usando notação de colchetes, atraves de seu index
+let ourArray = ["a", "b", "c"]; //podemos acessar os indices 0,1,2 e tambes modificalos posteriormente
+console.log(ourArray[1]);
+ourArray[1] = "victor";
+console.log(ourArray[1])
+//
+
