8d0ee53a43e5f633f74386e118c8c9a6c1432df9
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-06-13 11:10:51 -0300

    Create Lab07Application.java

---
+++ WEB-UFC/lab07-web/Lab-07/src/main/java/br/ufc/web/Lab07/Lab07Application.java
@@ -0,0 +1,13 @@
+package br.ufc.web.Lab07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab07Application.class, args);
+	}
+
+}
